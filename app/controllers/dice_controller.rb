3a31d983f980c1c16de6a73e35ce74dc84b4ea1c
yepes0 <71985093+yepes0@users.noreply.github.com>
2025-08-11 03:56:26 +0000

    I think i've made some progress

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -46,4 +46,19 @@ class DiceController < ApplicationController
 
     render({ :template => "dice_templates/result_five_four" })
   end
+
+  def roll_flexible
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    
+
+    @rolls = []
+
+    @num_dice.times do
+      dice = rand(1..@sides)
+      @rolls.push(dice)
+    end
+
+    render({ :template => "dice_templates/result_flexible" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get("/dice/2/10", { :controller => "dice", :action => "roll_two_ten" })
   get("/dice/1/20", { :controller => "dice", :action => "roll_one_twenty" })
   get("/dice/5/4", { :controller => "dice", :action => "roll_five_four" })
+
+  get("/dice/:number_of_dice/:how_many_sides", { :controller => "dice", :action => "roll_flexible" })
 end
